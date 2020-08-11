fb26db1665f0a62ddcf53c1711ba3b10ba9d6c8c
Konrad <k.jachimstal@gmail.com>
2020-08-11 13:56:11 +0200

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,8 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'userable_type' => $faker->randomElement([
-            App\Doctor::class
+            App\Doctor::class, 
+            App\Patient::class
         ]),
         'userable_id' => function(array $user) {
             return factory($user['userable_type']);
