bdbef6b2cea86a640889b3e2b600c7b407bc9bf8
Konrad <k.jachimstal@gmail.com>
2020-08-11 14:28:14 +0200

    Add Employee

--- app/User.php
+++ app/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
     public function getIsDoctorAttribute() {
         return $this->userable_type == Doctor::class;
     }
+
+    public function getIsEmployeeAttribute() {
+        return $this->userable_type == Employee::class;
+    }
 }
