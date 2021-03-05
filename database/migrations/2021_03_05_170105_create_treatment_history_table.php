b412f3a1871f2afdb193dcbf253fa671ec96fee9
KJachimstal <k.jachimstal@gmail.com>
2021-03-05 18:11:28 +0100

    Create table for treatmet history

---
+++ database/migrations/2021_03_05_170105_create_treatment_history_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreatmentHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatment_history', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('visit_date');
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->foreignId('doctor_speciality_id')->constrained('doctor_speciality')->onDelete('cascade');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treatment_history');
+    }
+}
