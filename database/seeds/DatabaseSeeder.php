15b0543b852035d2dc7652968648bc6d3c55c0ba
KJachimstal <k.jachimstal@gmail.com>
2021-03-12 16:50:27 +0100

    Details seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SpecialitySeeder::class);
         $this->call(DoctorSpecialitySeeder::class);
         $this->call(AppointmentSeeder::class);
+        // $this->call(DetailSeeder::class);
     }
 }
